32fb06972e5e09995fa78eb2aaf2b7e5f37e8a61
tmxkqotnl <tmxkqotnl@gmail.com>
2021-04-27 23:50:30 +0900

    lots of

---
+++ script/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('#welcome').click(function(){
+        $(this).animate({
+            opacity:0,
+        },500,()=>{
+            $(this).text("Hello, Stranger");
+            $(this).animate({
+                opacity:1,
+            },1000,function(){
+                $('.content').animate({
+                    left:'-100vw',
+                },1500,()=>{
+                    $(this).css('left','-100vw');
+                });
+                $('.video-frame').animate({
+                    opacity:0,
+                },1500,()=>{
+                    $(this).css('opacity',0);
+                    $('.tags').animate({
+                        opacity:1,
+                    },2000,function(){
+                        $(this).css('opacity',1);
+                    });    
+                });
+                
+            });
+        });
+        
+        
+    });
+});
