b116e1aff5f6da78e6244ca42c61d453b36494b3
tmxkqotnl <tmxkqotnl@gmail.com>
2021-04-30 19:18:16 +0900

    fixed

--- script/index.js
+++ script/index.js
@@ -1,31 +1,46 @@
-$(document).ready(function(){
-    $('#welcome').click(function(){
-        $(this).animate({
-            opacity:0,
-        },500,()=>{
-            $(this).text("Hello, Stranger");
-            $(this).animate({
-                opacity:1,
-            },1000,function(){
-                $('.content').animate({
-                    left:'-100vw',
-                },1500,()=>{
-                    $(this).css('left','-100vw');
-                });
-                $('.video-frame').animate({
-                    opacity:0,
-                },1500,()=>{
-                    $(this).css('opacity',0);
-                    $('.tags').animate({
-                        opacity:1,
-                    },2000,function(){
-                        $(this).css('opacity',1);
-                    });    
-                });
-                
+$(document).ready(function () {
+    var cur = 0;
+    //start
+    setTimeout(function () {
+        $('.welcome').animate({
+            opacity: 0,
+        }, 700, function () {
+            $('.welcome').css('visibility', 'hidden');
+            $('.container').animate({
+                top: 0,
+            }, 300, () => {
+                $('.container').css('top', 0);
+                $('.container').animate({
+                    opacity: 1,
+                }, 1000);
+                $('.vertical').css('opacity', 1);
             });
         });
-        
-        
+        $('.black').animate({
+            left: '110%',
+        }, 500, function () {
+            $('.black').css('left', '100%');
+        });
+    }, 1000);
+
+    //button
+    $('#up').click(function () {
+        if (cur < 0) {
+            cur += 100;
+            $('.container').animate({
+                top: `${cur}vh`,
+            }, 1000, function () {
+            });
+        }
+    });
+    $('#down').click(function () {
+        if (cur > -200) {
+            cur -= 100;
+            let temp = `${cur}vh`;
+            $('.container').animate({
+                top: temp,
+            }, 1000, function () {
+            });
+        }
     });
 });
